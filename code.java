51f6f4505d5199df427b6c30a7fce23548f38b91
Raviraj <raviraj.gurav@infor.com>
2017-12-29 18:50:36 +0530

    commited chaned

--- code.java
+++ code.java
@@ -1,8 +1,10 @@
-public class code{
-public static void main(String[] args)
-    {
-        for(int i = 1; i <= 10; i++)
-        {
-            System.out.println(i);
-        }
-}}
+public class code
+{
+	public static void main(String[] args)
+    		{
+        		for(int i = 1; i <= 10; i++)
+		        {
+		            System.out.println(i);
+		        }
+		}
+}
